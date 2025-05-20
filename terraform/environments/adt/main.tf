7d214b83c8b65e5f50eb58851bc530cf00f98b5a
Najeev Nambiar <nnambiar@costco.com>
2025-05-20 15:33:39 -0700

    add bare min

---
+++ terraform/environments/adt/main.tf
@@ -0,0 +1,54 @@
+provider "google" {
+  project = var.projectId
+}
+
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      # Ensure version is at least 4.22.0 - released May 2022
+      version = ">= 4.22.0"
+    }
+    archive = {}
+  }
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "gcp-gcs-cicd-core-mbr-bc-lead"
+    prefix = "adt"
+  }
+}
+
+
+resource "google_storage_bucket" "this" {
+  name          = "cs-${var.prefix}-${var.country}-${var.environment}"
+  location      = var.location
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+  public_access_prevention    = "enforced"
+  project                     = var.projectId
+  labels        = var.labels
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    condition {
+      days_since_noncurrent_time = 7
+    }
+    action {
+      type = "Delete"
+    }
+  }
+  lifecycle_rule {
+    condition {
+      with_state = "ARCHIVED"
+      num_newer_versions = 2
+    }
+    action {
+      type = "Delete"
+    }
+  }
+}
---
+++ terraform/environments/adt/variables.tf
@@ -0,0 +1,25 @@
+variable "projectId" { 
+  description = "The ID of the GCP Project"
+  type = string 
+  default     = "p-601-np-membership-adt"  # Default for ADT project
+}
+
+variable "environment" { 
+  description = "GCP environment"
+  type = string 
+  default     = "adt"  # Default for ADT project
+}
+
+variable "location" {
+  description = "The geographic location for the dataset"
+  type        = string
+  default     = "US"
+}
+
+variable "labels" {
+  description = "Resource labels"
+  type        = map(string)
+  default = {
+    env = "adt"
+  }
+}
