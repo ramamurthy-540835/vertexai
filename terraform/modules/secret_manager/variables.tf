c8b90daf6997212eeba62132c18a6e436ff458fd
Upasana Pandey [upandey] <c_upandey@costco.com>
2025-05-22 17:26:11 +0530

    Create variables.tf for secret manager

---
+++ terraform/modules/secret_manager/variables.tf
@@ -0,0 +1,12 @@
+variable "project_id" {
+  description = "Project information"
+  type        = string
+}
+variable "secret_id" {
+  description = "secret id information"
+  type        = string
+}
+variable "secret_value" {
+  description = "secret value information"
+  type        = string
+}
