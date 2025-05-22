5f28eb584e7ac9a8ea8eae16458f7978ae430291
Ragul Thirupathi [rthirupathi] <c_rthirupathi@costco.com>
2025-05-22 23:58:18 +0530

    Updated variables.tf

--- terraform/modules/secret_manager/variables.tf
+++ terraform/modules/secret_manager/variables.tf
@@ -6,7 +6,4 @@ variable "secret_id" {
   description = "secret id information"
   type        = string
 }
-variable "secret_value" {
-  description = "secret value information"
-  type        = string
-}
+
