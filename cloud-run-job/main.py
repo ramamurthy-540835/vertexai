2abbc18981afd569c9ae27295657857d627b13a9
Ragul Thirupathi [rthirupathi] <c_rthirupathi@costco.com>
2025-05-23 17:07:51 +0530

    Updated and rename main.py to run.py for cloud run job

--- cloud-run-job/main.py
+++
@@ -1,9 +0,0 @@
-import time
-
-print("Starting Cloud Run Job...")
-
-for i in range(5):
-    print(f"Processing step {i + 1}/5")
-    time.sleep(1)
-
-print("Cloud Run Job completed.")
---
+++ cloud-run-job/run.py
@@ -0,0 +1,52 @@
+from google.cloud import aiplatform
+from costco.leadmgmt.pipeline_manager.compile_pipeline import compile_and_upload_pipeline
+from costco.leadmgmt.util.match_id_creation import match_id_creation
+from costco.leadmgmt.components.update_source_data import update_cloud_sql
+import sys
+import os
+
+
+def run_pipeline(config_file_path):
+
+    project_id = os.environ.get("PROJECT_ID")
+    region = os.environ.get("REGION")
+    pipeline_name = os.environ.get("PIPELINE_NAME")
+    vertex_ai_network = os.environ.get("VERTEX_AI_NETWORK")
+    service_account = os.environ.get("SERVICE_ACCOUNT")
+    registry_url = os.environ.get("ARTIFACT_REGISTRY_URL")
+
+    aiplatform.init(project=project_id, location=region)
+    template_path = f"{registry_url}/{pipeline_name}/latest"
+
+    match_id = str(match_id_creation())
+
+
+
+    job = aiplatform.PipelineJob(
+        display_name=f"{pipeline_name}-latest",
+        template_path=template_path,
+        parameter_values={
+            'match_id': match_id,
+            'config_file_path': config_file_path
+        }
+    )
+
+    job.run(network=vertex_ai_network, service_account=service_account, sync=True)
+    print("Pipeline executed successfully.")
+
+if __name__ == "__main__":
+    stage = sys.argv[1]
+
+    config_file_path = os.environ.get("CONFIG_FILE_PATH")
+    pipeline_name = os.environ.get("PIPELINE_NAME")
+    registry_url = os.environ.get("ARTIFACT_REGISTRY_URL")
+
+    if stage.lower() == "compile_run_pipeline":
+        compile_and_upload_pipeline(pipeline_name,registry_url)
+        run_pipeline(config_file_path)
+    elif stage.lower() == "run_pipeline":
+        run_pipeline(config_file_path)
+    elif stage.lower() == "compile_pipeline":
+        compile_and_upload_pipeline(pipeline_name, registry_url)
+    elif stage.lower() == "update_database":
+        update_cloud_sql(config_file_path)
