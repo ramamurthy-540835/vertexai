6c5ad935d7a28ad7c7d17b74fa2a47e0f1d355dc
Upasana Pandey [upandey] <c_upandey@costco.com>
2025-05-23 13:11:40 +0530

    Update main.tf

--- terraform/modules/secret_manager/main.tf
+++ terraform/modules/secret_manager/main.tf
@@ -2,16 +2,14 @@
 resource "google_secret_manager_secret" "this" {
   project   = var.project_id
   secret_id = var.secret_id
-  secret_value = var.secret_value
 
   replication {
     automatic = true
   }
 }
 
-# Creating secret version with service account key
+# Creating secret version
 resource "google_secret_manager_secret_version" "this" {
   secret = google_secret_manager_secret.this.id
-
-  secret_data = google_service_account_key.this.secret_value
+  secret_data = var.secret_value
 }
