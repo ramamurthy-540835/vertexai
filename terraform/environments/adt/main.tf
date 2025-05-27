c917d94704445f3d99206a9e219811a865ac97e0
Upasana Pandey [upandey] <c_upandey@costco.com>
2025-05-27 12:28:11 +0530

    Update main.tf
    Added the database module in main.tf

--- terraform/environments/adt/main.tf
+++ terraform/environments/adt/main.tf
@@ -83,3 +83,16 @@ module "service_now_password" {
  secret_id     = "lead_mgmt_snow_password"
  secret_value  = "super-secret-value"
 }
+
+module "cloud_sql_instance" {
+  source            = "../../modules/database"
+
+  instance_name     = "lead_mgmt_dev"
+  version           = "POSTGRES_16"
+  region            = "us-central1"
+  tier              = "db-custom-4-16384"
+  edition           = "ENTERPRISE"
+  availability_type = "REGIONAL"
+  activation_policy = "ALWAYS"
+  disk_size         = 100
+}
