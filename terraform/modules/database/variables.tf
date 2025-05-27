aef7550f2cf1a3e49734878e9d567c1a1bbf2a00
Upasana Pandey [upandey] <c_upandey@costco.com>
2025-05-27 12:23:43 +0530

    Create variables.tf
    added variables.tf for this database creation script

---
+++ terraform/modules/database/variables.tf
@@ -0,0 +1,43 @@
+variable "instance_name" {
+  description = "The name of the Cloud SQL instance"
+  type        = string
+}
+
+variable "version" {
+  description = "The database engine type and version"
+  type        = string
+}
+
+variable "region" {
+  description = "The region of the Cloud SQL instance"
+  type        = string
+}
+
+variable "tier" {
+  description = "The tier for the database instance"
+  type        = string
+}
+
+variable "edition" {
+  description = "The edition of the SQL instance (e.g., ENTERPRISE)"
+  type        = string
+  default     = "ENTERPRISE"
+}
+
+variable "availability_type" {
+  description = "Specifies whether the instance should be zonal or regional"
+  type        = string
+  default     = "ZONAL"
+}
+
+variable "activation_policy" {
+  description = "The activation policy for the instance (ALWAYS, NEVER, ON_DEMAND)"
+  type        = string
+  default     = "ALWAYS"
+}
+
+variable "disk_size" {
+  description = "The size of data disk in GB"
+  type        = number
+  default     = 10
+}
