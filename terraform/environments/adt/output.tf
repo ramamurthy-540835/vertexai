7173adf259306527db79253d1f76bac65c258728
Upasana Pandey [upandey] <c_upandey@costco.com>
2025-05-28 14:04:05 +0530

    Update output.tf

--- terraform/environments/adt/output.tf
+++ terraform/environments/adt/output.tf
@@ -2,3 +2,7 @@ output "bucket_name" {
   description = "Fully qualified cs bucket name"
   value       = google_storage_bucket.this.name
 }
+
+output "scheduler_job_name" {
+  value = google_cloud_scheduler_job.snow_sync_scheduler.name
+}
