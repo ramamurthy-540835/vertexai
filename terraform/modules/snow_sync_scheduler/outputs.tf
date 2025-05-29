320b36c95e6d5ff7fe401107286b2b5d37cb5ea8
upandey_costco <c_upandey@costco.com>
2025-05-29 18:51:44 +0000

    Updated module path in main.tf

--- terraform/modules/snow_sync_scheduler/outputs.tf
+++ terraform/modules/snow_sync_scheduler/outputs.tf
@@ -3,7 +3,7 @@ output "pubsub_topic_id" {
   value       = google_pubsub_topic.snow_sync_trigger.id
 }
 
-#output "scheduler_job_name" {
-#  description = "Name of the created Cloud Scheduler job."
-#  value       = google_cloud_scheduler_job.snow_sync_scheduler.name
-#}
+output "scheduler_job_name" {
+  description = "Name of the created Cloud Scheduler job."
+  value       = google_cloud_scheduler_job.snow_sync_scheduler.name
+}
