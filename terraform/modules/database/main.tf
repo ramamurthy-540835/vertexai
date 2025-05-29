4b0d3b93b8b452b591586c0bb0e16f656d4d5895
Upasana Pandey [upandey] <c_upandey@costco.com>
2025-05-29 18:15:19 +0530

    Update main.tf

--- terraform/modules/database/main.tf
+++ terraform/modules/database/main.tf
@@ -2,7 +2,7 @@ resource "google_sql_database_instance" "this" {
   name             = var.instance_name
   database_version = var.database_version
   region           = var.region
-  project          = var.project_id
+  project          = var.project
 
   settings {
     # Second-generation instance tiers are based on the machine
