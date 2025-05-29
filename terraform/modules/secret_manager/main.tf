431736bb4fb0b7a39c019376ad1aa93419d26d57
Upasana Pandey [upandey] <c_upandey@costco.com>
2025-05-29 13:30:28 +0530

    Update main.tf

--- terraform/modules/secret_manager/main.tf
+++ terraform/modules/secret_manager/main.tf
@@ -2,7 +2,16 @@
 resource "google_secret_manager_secret" "this" {
   project   = var.project
   secret_id = var.secret_id
+
+ replication {
+    user_managed {
+      replicas {
+        location = "us-central1"
+      }
+    }
+  }
 }
+
 
 # Creating secret version
 resource "google_secret_manager_secret_version" "this" {
