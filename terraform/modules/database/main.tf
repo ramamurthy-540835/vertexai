d15c4326a73e2eb24155c365423165e4d12c72d9
Upasana Pandey [upandey] <c_upandey@costco.com>
2025-06-03 17:08:12 +0530

    Update main.tf

--- terraform/modules/database/main.tf
+++ terraform/modules/database/main.tf
@@ -1,49 +1,49 @@
-resource "google_sql_database_instance" "this" {
-  name             = var.instance_name # Must use only lowercase, numbers, hyphens
-  project          = var.project
-  database_version = var.database_version # Should be a supported MySQL or PostgreSQL version for Enterprise Plus
-  region           = var.region
-
-  settings {
-    tier    = var.tier
-
-    # Must be "ENTERPRISE_PLUS" to use data_cache_config
-    edition = var.edition
-
-    # Only include this block for Enterprise Plus and supported engines
-    #data_cache_config {
-    # data_cache_enabled = true
-    #}
-
-    backup_configuration {
-      enabled = true
-      # Uncomment only if using MySQL
-      # binary_log_enabled = true
-    }
-
-    availability_type = var.availability_type
-    activation_policy = var.activation_policy
-    deletion_protection_enabled = true
-    disk_autoresize = true
-    disk_size = var.disk_size
-
-    insights_config {
-      query_insights_enabled = true
-    }
-
-    # Ensure this flag is compatible with your engine/version
-    database_flags {
-      name  = "cloudsql.iam_authentication"
-      value = "on"
-    }
-  }
-}
-
-resource "google_sql_user" "iam_service_account_user" {
-  # Note: for Postgres only, GCP requires omitting the ".gserviceaccount.com" suffix
-  # from the service account email due to length limits on database usernames.
-  project  = var.project
-  name     = trimsuffix(var.service_account, ".gserviceaccount.com")
-  instance = google_sql_database_instance.this.name
-  type     = "CLOUD_IAM_SERVICE_ACCOUNT"
-}
+#resource "google_sql_database_instance" "this" {
+#  name             = var.instance_name # Must use only lowercase, numbers, hyphens
+#  project          = var.project
+#  database_version = var.database_version # Should be a supported MySQL or PostgreSQL version for Enterprise Plus
+#  region           = var.region
+#
+#  settings {
+#    tier    = var.tier
+#
+#    # Must be "ENTERPRISE_PLUS" to use data_cache_config
+#    edition = var.edition
+#
+#    # Only include this block for Enterprise Plus and supported engines
+#    #data_cache_config {
+#    # data_cache_enabled = true
+#    #}
+#
+#    backup_configuration {
+#      enabled = true
+#      # Uncomment only if using MySQL
+#      # binary_log_enabled = true
+#    }
+#
+#    availability_type = var.availability_type
+#    activation_policy = var.activation_policy
+#    deletion_protection_enabled = true
+#    disk_autoresize = true
+#    disk_size = var.disk_size
+#
+#    insights_config {
+#      query_insights_enabled = true
+#    }
+#
+#    # Ensure this flag is compatible with your engine/version
+#    database_flags {
+#      name  = "cloudsql.iam_authentication"
+#      value = "on"
+#    }
+#  }
+#}
+#
+#resource "google_sql_user" "iam_service_account_user" {
+#  # Note: for Postgres only, GCP requires omitting the ".gserviceaccount.com" suffix
+#  # from the service account email due to length limits on database usernames.
+#  project  = var.project
+#  name     = trimsuffix(var.service_account, ".gserviceaccount.com")
+#  instance = google_sql_database_instance.this.name
+#  type     = "CLOUD_IAM_SERVICE_ACCOUNT"
+#}
