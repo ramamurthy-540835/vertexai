99e60d71c286208cc60e6ddd96f3f78484ab5547
upandey_costco <c_upandey@costco.com>
2025-06-06 09:03:12 +0000

    Added subnet for database

--- terraform/modules/database/main.tf
+++ terraform/modules/database/main.tf
@@ -34,7 +34,7 @@ resource "google_sql_database_instance" "this" {
 
      ip_configuration {
       ipv4_enabled    = false
-      private_network = data.google_compute_subnetwork.my-subnetwork.self_link
+      private_network = data.google_compute_subnetwork.my-subnetwork.network
       ssl_mode = "ENCRYPTED_ONLY"
     }
 
