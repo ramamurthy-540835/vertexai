7a949dc7d174398efc39c2af02da0b10252ed788
upandey_costco <c_upandey@costco.com>
2025-06-12 19:39:19 +0000

    Added sql scripts

--- terraform/environments/adt/schema/main.tf
+++ terraform/environments/adt/schema/main.tf
@@ -24,45 +24,23 @@ terraform {
   host     = var.host
   port     = var.port
   database = var.database_name
-  username = var.gcp_workload_identity_sa_email
+  username = "gco-iam-svc-cicd-mbr-bc-np@gcp-prj-cicd-core.iam"
   sslmode  = "require"
 }
 
-resource "null_resource" "execute_sql_scripts" {
- triggers = {
-   sql_script_hash = filesha256("../../../../postgres_resources/lead_mgmt_schema_creation.sql")
- }
- provisioner "local-exec" {
-   command = <<-EOT
-     # Get access token for IAM authentication
-     export PGPASSWORD=$(gcloud auth print-access-token)
-     # Connect using private IP with IAM authentication
-     psql "host=${var.host}\
-           port=5432 \
-           dbname=${var.database_name} \
-           user=${var.gcp_workload_identity_sa_email} \
-           sslmode=require" \
-           -f "../../../../postgres_resources/lead_mgmt_schema_creation.sql"
-   EOT
-   environment = {
-     CLOUDSQL_INSTANCE = "${var.projectId}:${var.region}:lead_mgmt_adt"
-   }
- }
 
-}
-
-# Execute SQL scripts directly
-#resource "postgresql_database" "execute_sql_scripts" {
-#  for_each = var.sql_scripts
+ # Execute SQL scripts directly
+resource "postgresql_database" "execute_sql_scripts" {
+  for_each = var.sql_scripts
   
-#  name = each.key
-#  template = "template0"
+  name = each.key
+  template = "template0"
   
-#  connection_limit = -1
-#  allow_connections = true
+  connection_limit = -1
+  allow_connections = true
   
   # This is a workaround - we'll use the lifecycle to run SQL
-#  lifecycle {
-#    ignore_changes = all
-#  }
-# }
+  lifecycle {
+    ignore_changes = all
+  }
+ }
