ed766bbc1882a6725d8745f805539abfc2635128
upandey_costco <c_upandey@costco.com>
2025-06-27 10:07:35 +0000

    Added changes in postgres scripts

--- postgres_resources/costco_db_dml.sql
+++ postgres_resources/costco_db_dml.sql
@@ -1,14 +1,14 @@
 
-INSERT INTO $SCHEMA_NAME.match_configuration (confidence_level, min_score, max_score)
+INSERT INTO "$SCHEMA_NAME".match_configuration (confidence_level, min_score, max_score)
 VALUES ('High', '90', '100');
 
-INSERT INTO $SCHEMA_NAME.match_configuration (confidence_level, min_score, max_score)
+INSERT INTO "$SCHEMA_NAME".match_configuration (confidence_level, min_score, max_score)
 VALUES ('Medium','85','89.999');
  
-INSERT INTO $SCHEMA_NAME.match_configuration (confidence_level, min_score, max_score)
+INSERT INTO "$SCHEMA_NAME".match_configuration (confidence_level, min_score, max_score)
 VALUES ('Low','80','84.999');
 
-INSERT INTO $SCHEMA_NAME.match_configuration (confidence_level, min_score, max_score)
+INSERT INTO "$SCHEMA_NAME".match_configuration (confidence_level, min_score, max_score)
 VALUES ('No Match','0','79.999');
  
 commit;
