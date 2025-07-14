5c7576a82ccc40d4f9066ffa50f264a81d5ed91c
upandey_costco <c_upandey@costco.com>
2025-07-14 13:34:40 +0000

    Updated the userid for iam

--- cloud_run_service/main.py
+++ cloud_run_service/main.py
@@ -24,4 +24,4 @@ def hello():
     return "Done!"
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
--- lead_management_job/main.py
+++ lead_management_job/main.py
@@ -56,9 +56,9 @@ if __name__ == "__main__":
         
         # Configuration
         INSTANCE_CONNECTION_NAME = "p-601-np-bcleadsmgmt-adt:us-central1:lead-mgmt-adt"
-        #DB_USER = "gco-iam-svc-cicd-mbr-bc-np@gcp-prj-cicd-core.iam"   # This must match your IAM identity
-        DB_USER = "postgres"
-        PASS = "RV/0V6@39%jU"
+        DB_USER = "gco-iam-svc-lead-mgmt-bc-adt@p-601-np-bcleadsmgmt-adt.iam"   # This must match your IAM identity
+        # DB_USER = "postgres"
+        # PASS = "RV/0V6@39%jU"
         DB_NAME = "lead-mgmt-db"
         PRIVATE_IP = "true"
         
