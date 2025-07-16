5d6fa063ea8c5c54aa516ac6880b6c75e79df874
upandey_costco <c_upandey@costco.com>
2025-07-16 10:08:04 +0000

    Added legacy role for gcs bucket

--- terraform/environments/adt/infra/main.tf
+++ terraform/environments/adt/infra/main.tf
@@ -34,8 +34,7 @@ resource "google_project_service" "required_apis_recreate" {
     "storage.googleapis.com",
     "cloudscheduler.googleapis.com",
     "servicenetworking.googleapis.com",
-    "run.googleapis.com",
-    "containerthreatdetection.googleapis.com"
+    "run.googleapis.com"
   ]) 
 
   project = var.projectId
