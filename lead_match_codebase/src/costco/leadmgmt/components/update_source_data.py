7f4eb28c2ef3dde8ee7b7a3c5ba0a215af64e79b
upandey_costco <c_upandey@costco.com>
2026-03-18 01:40:31 +0000

    Fixed null value assignment

--- lead_match_codebase/src/costco/leadmgmt/components/update_source_data.py
+++ lead_match_codebase/src/costco/leadmgmt/components/update_source_data.py
@@ -6,6 +6,7 @@ from costco.leadmgmt.config.Configuration import JobConfig
 from costco.leadmgmt.util.apputil import load_file_from_gcs
 from google.cloud import storage
 from sqlalchemy.types import TIMESTAMP
+import numpy as np
 
 def get_gcs_file_path(uri: str) -> str:
     if not uri.startswith("gs://"):
@@ -128,8 +129,8 @@ def update_cloud_sql(config_file_path: str,file_path: str = ""):
     # preprocessing leads_dataframe
     # Assign 'closed_fiscal_period' and 'closed_fiscal_year' for 'High' confidence level
     leads_dataframe = final_df[final_df['confidence_level'].isin(['High', 'Medium', 'Low'])]
-    leads_dataframe.loc[:, 'closed_fiscal_period'] = None
-    leads_dataframe.loc[:, 'closed_fiscal_year'] = None
+    leads_dataframe.loc[:, 'closed_fiscal_period'] = np.nan
+    leads_dataframe.loc[:, 'closed_fiscal_year'] = np.nan
     high_confidence = leads_dataframe[leads_dataframe['confidence_level'] == 'High']
 
     leads_dataframe.loc[high_confidence.index, 'closed_fiscal_period'] = 8
