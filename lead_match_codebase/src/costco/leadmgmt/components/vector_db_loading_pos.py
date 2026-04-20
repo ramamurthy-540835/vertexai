bf0221a3dc805c5c36efabde09b9097750346217
Upasana Pandey <upandey@corp.costco.com>
2026-04-20 15:34:52 +0530

    Removed integer conversion from max worker attribute

--- lead_match_codebase/src/costco/leadmgmt/components/vector_db_loading_pos.py
+++ lead_match_codebase/src/costco/leadmgmt/components/vector_db_loading_pos.py
@@ -115,9 +115,9 @@ def process_in_batch(df, embedding_column_name, column_name):
 
     # Flatten and assign
     all_embeddings = [emb for batch in results if batch for emb in batch]
-    # df_to_embed[embedding_column_name] = all_embeddings
+    df_to_embed[embedding_column_name] = all_embeddings
 
-    # df.update(df_to_embed)
+    df.update(df_to_embed)
     df.loc[df_to_embed.index, embedding_column_name] = all_embeddings
 
     df[embedding_column_name] = df[embedding_column_name].apply(
