d08150a7b43ce17a2cc8ddfd8849f94ea932d7e9
Upasana Pandey <upandey@corp.costco.com>
2026-05-08 14:48:00 +0530

    removed additional module from event arc setup

--- terraform/modules/event_arc/main.tf
+++ terraform/modules/event_arc/main.tf
@@ -1,46 +1,4 @@
 # ─────────────────────────────────────────────────────────────
-# Data Sources
-# ─────────────────────────────────────────────────────────────
-
-data "google_project" "project" {
-  project_id = var.project_id
-}
-
-data "google_storage_project_service_account" "gcs_sa" {
-  project = var.project_id
-}
-
-# ─────────────────────────────────────────────────────────────
-# IAM Permissions
-# ─────────────────────────────────────────────────────────────
-
-# Allow GCS service account to publish events
-resource "google_pubsub_topic_iam_member" "gcs_pubsub_publisher" {
-  project = var.project_id
-  topic   = google_pubsub_topic.eventarc_transport.name
-  role   = "roles/pubsub.publisher"
-  member = "serviceAccount:${data.google_storage_project_service_account.gcs_sa.email_address}"
-}
-
-# Allow Eventarc service agent to publish to transport topic
-resource "google_pubsub_topic_iam_member" "eventarc_publisher" {
-  project = var.project_id
-  topic   = google_pubsub_topic.eventarc_transport.name
-  role    = "roles/pubsub.publisher"
-
-  member = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-eventarc.iam.gserviceaccount.com"
-}
-
-# ─────────────────────────────────────────────────────────────
-# Transport Pub/Sub Topic
-# ─────────────────────────────────────────────────────────────
-
-resource "google_pubsub_topic" "eventarc_transport" {
-  name    = "${var.trigger_name}-transport"
-  project = var.project_id
-}
-
-# ─────────────────────────────────────────────────────────────
 # Eventarc Trigger
 # ─────────────────────────────────────────────────────────────
 
@@ -64,15 +22,4 @@ resource "google_eventarc_trigger" "gcs_workflow_trigger" {
   destination {
     workflow = "projects/${var.project_id}/locations/${var.region}/workflows/${var.workflow_name}"
   }
-
-  transport {
-    pubsub {
-      topic = google_pubsub_topic.eventarc_transport.id
-    }
-  }
-
-  depends_on = [
-  google_pubsub_topic_iam_member.gcs_pubsub_publisher,
-  google_pubsub_topic_iam_member.eventarc_publisher
-]
 }
