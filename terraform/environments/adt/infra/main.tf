69b2465a6b0fddf1f404537686b5f6d127173c2c
Upasana Pandey <upandey@corp.costco.com>
2026-05-09 23:31:16 +0530

    Added new workflow for testing lead match job

--- terraform/environments/adt/infra/main.tf
+++ terraform/environments/adt/infra/main.tf
@@ -216,7 +216,7 @@ module "gcs_eventarc_workflow_trigger" {
   location              = var.region
   trigger_name          = "pos-manifest-trigger"
   pubsub_topic_id       = module.gcs_pubsub_trigger.topic_id
-  workflow_name         = "lead_match_workflow"
+  workflow_name         = "snow_sync_workflow"
   workflow_location     = var.region
   service_account_email = module.project_init.service_account_email
 }
