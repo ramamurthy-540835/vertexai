fbc2736037fb0a170d74117c46f3bda6529eb586
Upasana Pandey <upandey@corp.costco.com>
2026-06-04 13:53:16 +0530

    Added cicd flow for prd deployment

--- postgres_resources/costco_db_dml.sql
+++ postgres_resources/costco_db_dml.sql
@@ -1,16 +1,11 @@
-
-INSERT INTO "$SCHEMA_NAME".match_configuration (confidence_level, min_score, max_score)
-VALUES ('High', '90', '100');
-
-INSERT INTO "$SCHEMA_NAME".match_configuration (confidence_level, min_score, max_score)
-VALUES ('Medium','85','89.999');
- 
-INSERT INTO "$SCHEMA_NAME".match_configuration (confidence_level, min_score, max_score)
-VALUES ('Low','80','84.999');
-
-INSERT INTO "$SCHEMA_NAME".match_configuration (confidence_level, min_score, max_score)
-VALUES ('No Match','0','79.999');
- 
-commit;
-
- 
+INSERT INTO "$SCHEMA_NAME".match_configuration (confidence_level, min_score, max_score, match_result)
+VALUES 
+  ('High',     90,      100,    'Match'),
+  ('Medium',   85,      89.999, 'Potential'),
+  ('Low',      80,      84.999, 'Potential'),
+  ('No Match',  0,      79.999, 'No Match')
+ON CONFLICT (confidence_level) 
+DO UPDATE SET
+  min_score    = EXCLUDED.min_score,
+  max_score    = EXCLUDED.max_score,
+  match_result = EXCLUDED.match_result;
