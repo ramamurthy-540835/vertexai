e7934746eaaef1ef1aa2a6977b7b81e366dc3674
Upasana Pandey <upandey@corp.costco.com>
2026-06-14 14:45:24 +0530

    force recreate monitoring channel

--- terraform/modules/monitoring_alert/main.tf
+++ terraform/modules/monitoring_alert/main.tf
@@ -27,10 +27,10 @@ resource "google_monitoring_alert_policy" "cloud_run_job_failure" {
       filter          = "metric.type=\"run.googleapis.com/job/completed_execution_count\" AND resource.type=\"cloud_run_job\" AND resource.label.\"job_name\"=\"lead-match-job\" AND metric.label.\"result\"=\"failed\""
       comparison      = "COMPARISON_GT"
       threshold_value = 0
-      duration        = "60s"
+      duration        = "0s"
       aggregations {
         alignment_period   = "60s"
-        per_series_aligner = "ALIGN_RATE"
+        per_series_aligner = "ALIGN_SUM"
       }
       trigger {
         count = 1
@@ -45,10 +45,10 @@ resource "google_monitoring_alert_policy" "cloud_run_job_failure" {
       filter          = "metric.type=\"run.googleapis.com/job/completed_execution_count\" AND resource.type=\"cloud_run_job\" AND resource.label.\"job_name\"=\"snow-sync-job\" AND metric.label.\"result\"=\"failed\""
       comparison      = "COMPARISON_GT"
       threshold_value = 0
-      duration        = "60s"
+      duration        = "0s"
       aggregations {
         alignment_period   = "60s"
-        per_series_aligner = "ALIGN_RATE"
+        per_series_aligner = "ALIGN_SUM"
       }
       trigger {
         count = 1
