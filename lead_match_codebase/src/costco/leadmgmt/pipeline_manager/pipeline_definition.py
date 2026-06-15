ed274635739da359177eb07622e0b5d037e58385
Upasana Pandey <upandey@corp.costco.com>
2026-06-15 23:12:07 +0530

    Test service now vertex ai connection

--- lead_match_codebase/src/costco/leadmgmt/pipeline_manager/pipeline_definition.py
+++ lead_match_codebase/src/costco/leadmgmt/pipeline_manager/pipeline_definition.py
@@ -66,6 +66,10 @@ def update_servicenow(config_file_path: str, file_path: str):
     from costco.leadmgmt.components.update_servicenow import update_servicenow
     return update_servicenow(config_file_path=config_file_path, file_path=file_path)
 
+@dsl.component(base_image=BASE_IMAGE)
+def check_connection():
+    from costco.leadmgmt.components.test_service_now_connectivity import check_connection
+    return check_connection()
 
 @dsl.pipeline(name=os.environ.get("PIPELINE_NAME"))
 def my_pipeline(
@@ -76,100 +80,102 @@ def my_pipeline(
 ):
     print("pipeline execution started")
 
-    track_failure_status = failure_handler(
-        match_id=match_id,
-        config_file_path=config_file_path
-    )
+    check_snow_status = check_connection()
+    check_snow_status.set_display_name("Check service Now connectivity")
+    # track_failure_status = failure_handler(
+    #     match_id=match_id,
+    #     config_file_path=config_file_path
+    # )
 
-    track_failure_status.set_display_name('Update failure status')
+    # track_failure_status.set_display_name('Update failure status')
 
-    with dsl.ExitHandler(exit_task=track_failure_status):
-        #Step 1: Load and preprocess the files from GCS
-        file_leads_preprocessed = load_and_preprocess_data_cloud_sql(
-            base_name="leads",
-            config_file_path=config_file_path
-        )
+    # with dsl.ExitHandler(exit_task=track_failure_status):
+    #     #Step 1: Load and preprocess the files from GCS
+    #     file_leads_preprocessed = load_and_preprocess_data_cloud_sql(
+    #         base_name="leads",
+    #         config_file_path=config_file_path
+    #     )
 
-        file_pos_preprocessed = load_and_preprocess_data_cloud_sql(
-            base_name="pos",
-            config_file_path=config_file_path
-        )
+    #     file_pos_preprocessed = load_and_preprocess_data_cloud_sql(
+    #         base_name="pos",
+    #         config_file_path=config_file_path
+    #     )
 
-        file_leads_preprocessed.set_caching_options(False).set_cpu_limit(CPU_LIMIT).set_memory_limit(MEMORY_LIMIT)
-        file_pos_preprocessed.set_caching_options(False).set_cpu_limit(CPU_LIMIT).set_memory_limit(MEMORY_LIMIT)
+    #     file_leads_preprocessed.set_caching_options(False).set_cpu_limit(CPU_LIMIT).set_memory_limit(MEMORY_LIMIT)
+    #     file_pos_preprocessed.set_caching_options(False).set_cpu_limit(CPU_LIMIT).set_memory_limit(MEMORY_LIMIT)
 
 
-        # step 2: Primary classification
+    #     # step 2: Primary classification
 
-        task_1 = primary_classification(
-            file_a_path=file_leads_preprocessed.output,  # Use the output of the preprocessed file task
-            file_b_path=file_pos_preprocessed.output,  # Use the output of the preprocessed file task
-            match_id=match_id,
-            config_file_path=config_file_path
-        )
-        task_1.set_caching_options(False).set_cpu_limit(CPU_LIMIT).set_memory_limit(MEMORY_LIMIT)
+    #     task_1 = primary_classification(
+    #         file_a_path=file_leads_preprocessed.output,  # Use the output of the preprocessed file task
+    #         file_b_path=file_pos_preprocessed.output,  # Use the output of the preprocessed file task
+    #         match_id=match_id,
+    #         config_file_path=config_file_path
+    #     )
+    #     task_1.set_caching_options(False).set_cpu_limit(CPU_LIMIT).set_memory_limit(MEMORY_LIMIT)
 
-        # step 3: Embedding Generation
+    #     # step 3: Embedding Generation
 
-        # Embedding generation leads
-        task_2 = embedding_generation_leads(
-            file_leads=file_leads_preprocessed.output,
-            config_file_path=config_file_path,
-            project_id=project_id,
-            max_workers=max_workers)
-        task_2.set_caching_options(False).set_cpu_limit(CPU_LIMIT_EG).set_memory_limit(MEMORY_LIMIT_EG)
+    #     # Embedding generation leads
+    #     task_2 = embedding_generation_leads(
+    #         file_leads=file_leads_preprocessed.output,
+    #         config_file_path=config_file_path,
+    #         project_id=project_id,
+    #         max_workers=max_workers)
+    #     task_2.set_caching_options(False).set_cpu_limit(CPU_LIMIT_EG).set_memory_limit(MEMORY_LIMIT_EG)
        
 
-        # Embedding generation pos
-        task_3 = embedding_generation_pos(
-            file_pos=file_pos_preprocessed.output,
-            config_file_path=config_file_path,
-            project_id=project_id,
-            max_workers=max_workers)
-        task_3.set_caching_options(False).set_cpu_limit(CPU_LIMIT_EG).set_memory_limit(MEMORY_LIMIT_EG)
+    #     # Embedding generation pos
+    #     task_3 = embedding_generation_pos(
+    #         file_pos=file_pos_preprocessed.output,
+    #         config_file_path=config_file_path,
+    #         project_id=project_id,
+    #         max_workers=max_workers)
+    #     task_3.set_caching_options(False).set_cpu_limit(CPU_LIMIT_EG).set_memory_limit(MEMORY_LIMIT_EG)
       
 
-        # step 4: fuzzy matching
-        task_4 = fuzzy_matching(
-            file_classified_path=task_1.output,
-            config_file_path=config_file_path).after(task_2, task_3)
+    #     # step 4: fuzzy matching
+    #     task_4 = fuzzy_matching(
+    #         file_classified_path=task_1.output,
+    #         config_file_path=config_file_path).after(task_2, task_3)
 
-        task_4.set_caching_options(False).set_cpu_limit(CPU_LIMIT).set_memory_limit(MEMORY_LIMIT)
+    #     task_4.set_caching_options(False).set_cpu_limit(CPU_LIMIT).set_memory_limit(MEMORY_LIMIT)
         
 
-        # step 5: Update source data
-        task_5 = update_cloud_sql(
-            config_file_path=config_file_path,
-            file_path=task_4.output).after(task_4)
+    #     # step 5: Update source data
+    #     task_5 = update_cloud_sql(
+    #         config_file_path=config_file_path,
+    #         file_path=task_4.output).after(task_4)
 
-        task_5.set_caching_options(False).set_cpu_limit(CPU_LIMIT).set_memory_limit(MEMORY_LIMIT)
+    #     task_5.set_caching_options(False).set_cpu_limit(CPU_LIMIT).set_memory_limit(MEMORY_LIMIT)
         
-        #step 6: Update service now
-        task_6 = update_servicenow(
-            config_file_path=config_file_path,
-            file_path=task_4.output
-        ).after(task_4)
+    #     #step 6: Update service now
+    #     task_6 = update_servicenow(
+    #         config_file_path=config_file_path,
+    #         file_path=task_4.output
+    #     ).after(task_4)
 
-        task_6.set_caching_options(False).set_cpu_limit(CPU_LIMIT).set_memory_limit(MEMORY_LIMIT)
+    #     task_6.set_caching_options(False).set_cpu_limit(CPU_LIMIT).set_memory_limit(MEMORY_LIMIT)
 
-        #Step 7: Cleanup the temporary files
-        task_7 = delete_temp_files_from_gcs(
-            match_id=match_id,
-            file_path=task_4.output,
-            config_file_path=config_file_path).after(task_5, task_6)
+    #     #Step 7: Cleanup the temporary files
+    #     task_7 = delete_temp_files_from_gcs(
+    #         match_id=match_id,
+    #         file_path=task_4.output,
+    #         config_file_path=config_file_path).after(task_5, task_6)
 
-        task_7.set_caching_options(False).set_cpu_limit(CPU_LIMIT).set_memory_limit(MEMORY_LIMIT)
+    #     task_7.set_caching_options(False).set_cpu_limit(CPU_LIMIT).set_memory_limit(MEMORY_LIMIT)
        
 
-        # Set display names for each stage
-        file_leads_preprocessed.set_display_name("Load and Preprocess Leads data")
-        file_pos_preprocessed.set_display_name("Load and Preprocess POS data")
-        task_1.set_display_name("Primary Classification")
-        task_2.set_display_name("Embedding and Leads Vector database Loading")
-        task_3.set_display_name("Embedding and PoS Vector database Loading")
-        task_4.set_display_name("Similarity Search")
-        task_5.set_display_name("Update matching results to cloud sql")
-        task_6.set_display_name("Update the matched results to ServiceNow")
-        task_7.set_display_name("Cleanup Temporary Files")
+    #     # Set display names for each stage
+    #     file_leads_preprocessed.set_display_name("Load and Preprocess Leads data")
+    #     file_pos_preprocessed.set_display_name("Load and Preprocess POS data")
+    #     task_1.set_display_name("Primary Classification")
+    #     task_2.set_display_name("Embedding and Leads Vector database Loading")
+    #     task_3.set_display_name("Embedding and PoS Vector database Loading")
+    #     task_4.set_display_name("Similarity Search")
+    #     task_5.set_display_name("Update matching results to cloud sql")
+    #     task_6.set_display_name("Update the matched results to ServiceNow")
+    #     task_7.set_display_name("Cleanup Temporary Files")
 
 
