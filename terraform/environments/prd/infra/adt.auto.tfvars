df7c6c506ba70d95c80784799db5c90bb900c512
Upasana Pandey <upandey@corp.costco.com>
2026-06-18 16:31:52 +0530

    Added changes in lead matching config

--- terraform/environments/prd/infra/adt.auto.tfvars
+++
@@ -1,4 +0,0 @@
-location	= "us" 
-environment	= "prd"
-projectId	= "p-601-pd-bcleadsmgmt-prd"
-region		= "us-central1"
--- terraform/environments/prd/infra/output.tf
+++
@@ -1,4 +0,0 @@
-output "bucket_name" {
-  description = "Fully qualified cs bucket name"
-  value       = module.main_bucket.bucket_name
-}
