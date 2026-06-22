5d48ca5a8bacd0af970020cff468095544aa7380
Ramamurthy <ramamurthy.valavandan@gmail.com>
2026-06-22 03:41:26 +0000

    Add Cloud SQL index maintenance to preflight workflow: schema/lead_match_hnsw_indexes.sql

--- schema/lead_match_hnsw_indexes.sql
+++ schema/lead_match_hnsw_indexes.sql
@@ -6,6 +6,22 @@
 
 \set ON_ERROR_STOP on
 
+ALTER TABLE leadmgmt.leads_embeddings
+    ALTER COLUMN combined_embedding TYPE vector(768) USING combined_embedding::vector(768),
+    ALTER COLUMN address_embedding TYPE vector(768) USING address_embedding::vector(768),
+    ALTER COLUMN name_embedding TYPE vector(768) USING name_embedding::vector(768);
+
+ALTER TABLE leadmgmt.pos_embeddings
+    ALTER COLUMN combined_embedding TYPE vector(768) USING combined_embedding::vector(768),
+    ALTER COLUMN address_embedding TYPE vector(768) USING address_embedding::vector(768),
+    ALTER COLUMN name_embedding TYPE vector(768) USING name_embedding::vector(768);
+
+CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS idx_leads_embeddings_lead_id_unique
+ON leadmgmt.leads_embeddings (lead_id);
+
+CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS idx_pos_embeddings_pos_id_unique
+ON leadmgmt.pos_embeddings (pos_id);
+
 CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_leads_embeddings_combined_hnsw
 ON leadmgmt.leads_embeddings
 USING hnsw (combined_embedding vector_cosine_ops)
